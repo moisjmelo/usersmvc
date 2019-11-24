08d1b1b951095a433714cc35726092e1ee34b47c
moisjmelo <moisjmelo@gmail.com>
2019-11-24 11:10:27 -0300

    Project created

---
+++ src/main/java/com/moisjmelo/usersmvc/UsersmvcApplication.java
@@ -0,0 +1,13 @@
+package com.moisjmelo.usersmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moisjmelo/usersmvc/UsersmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moisjmelo.usersmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
