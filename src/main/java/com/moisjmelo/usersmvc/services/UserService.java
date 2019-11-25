b67d4df29e4fba346333067e264e843260d29eba
moisjmelo <moisjmelo@gmail.com>
2019-11-24 22:57:47 -0300

    UserService + component registration

--- src/main/java/com/moisjmelo/usersmvc/services/UserService.java
+++ src/main/java/com/moisjmelo/usersmvc/services/UserService.java
@@ -1,22 +1,29 @@
 package com.moisjmelo.usersmvc.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.moisjmelo.usersmvc.entities.User;
 import com.moisjmelo.usersmvc.repositories.UserRepository;
 
+@Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
-		
-		
+
+	public List<User> findAll() {
+
 		return repository.findAll();
-		
+
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();		
 	}
 
 }
