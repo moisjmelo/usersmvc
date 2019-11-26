f185329fd8c4a3a6be03ba3c7813ff8f248822a4
moisjmelo <moisjmelo@gmail.com>
2019-11-26 08:41:28 -0300

    Payment one-to-one association

--- src/main/java/com/moisjmelo/usersmvc/config/TestConfig.java
+++ src/main/java/com/moisjmelo/usersmvc/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.moisjmelo.usersmvc.entities.Category;
 import com.moisjmelo.usersmvc.entities.Order;
 import com.moisjmelo.usersmvc.entities.OrderItem;
+import com.moisjmelo.usersmvc.entities.Payment;
 import com.moisjmelo.usersmvc.entities.Product;
 import com.moisjmelo.usersmvc.entities.User;
 import com.moisjmelo.usersmvc.enums.OrderStatus;
@@ -86,6 +87,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
 	}
 	
 	
