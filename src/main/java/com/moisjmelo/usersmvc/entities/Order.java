07ff4b19299f4cf63c80086c3d516dc91720a1bd
moisjmelo <moisjmelo@gmail.com>
2019-11-26 10:36:58 -0300

    SubTotal and Total

--- src/main/java/com/moisjmelo/usersmvc/entities/Order.java
+++ src/main/java/com/moisjmelo/usersmvc/entities/Order.java
@@ -59,31 +59,7 @@ public class Order implements Serializable {
 	public Long getId() {
 		return id;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+	
 
 	public void setId(Long id) {
 		this.id = id;
@@ -116,8 +92,7 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
-	
-	
+		
 	public Payment getPayment() {
 		return payment;
 	}
@@ -128,6 +103,39 @@ public class Order implements Serializable {
 
 	public Set<OrderItem> getItems(){
 		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
