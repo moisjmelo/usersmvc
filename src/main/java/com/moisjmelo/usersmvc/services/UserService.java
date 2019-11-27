d3232d583a1a79839253ddc19065e6b4e154eb21
moisjmelo <moisjmelo@gmail.com>
2019-11-26 22:24:18 -0300

    Exception handling - findById

--- src/main/java/com/moisjmelo/usersmvc/services/UserService.java
+++ src/main/java/com/moisjmelo/usersmvc/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.moisjmelo.usersmvc.entities.User;
 import com.moisjmelo.usersmvc.repositories.UserRepository;
+import com.moisjmelo.usersmvc.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();		
+		return obj.orElseThrow(( )-> new ResourceNotFoundException(id));		
 	}
 	
 	public User insert(User obj) {
